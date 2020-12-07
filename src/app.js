9555c76fed29751828c0fa458b34b982a5d121cf
@devojyoti1995 <devojyoti1995@gmail.com>
2020-12-07 22:33:36 +0530

    Committed

--- src/app.js
+++ src/app.js
@@ -1,18 +1,49 @@
-const subscribers = require('../src/models/subscribers.js')
+const subscribers = require('./models/subscribers')
 const express = require('express');
 const app = express()
 
 
 // Your code goes here
-app.get("/subscribers",(res,req)=>{
-    subscribers.findAll;
+app.get("/subscribers",async (req,res)=>{
+    res.send(await subscribers.find());
 })
-app.get("/subscribers/:id",(res,req)=>{
-    subscribers.findOne;
+// app.get("/subscribers/names",async (re,req)=>{
+//     const fullListOfSubscribers = await subscribers.find();
+//     const mappedResults = fullListOfSubscribers.map(subscriber => ({
+//         name : subscriber.name,
+//         subscribedChannel : subscriber.subscribedChannel
+//     }));
+
+//     res.send(mappedResults);
+// })
+
+
+app.get("/subscribers/names",async (re,req)=>{
+    const projectedResult = await subscribers.find().select({
+        _id : false,
+        subscribedDate : false,
+        __v : false
+        
+    });
+    res.send(projectedResult);
+   
 })
-app.get("/subscribers/names",(res,req)=>{
-    subscribers.find(subscribers).find(names);
-})
+
+
+app.get("/subscribers/:id",async (res,req)=>{
+    const idToSearch = req.params.id;
+    try{
+        const subscriberFound = await subscribers.findOne({_id:idToSearch});
+        if(subscriberFound==null){
+            res.status(400).send({messeage:"Id not found"});
+        }else{
+            res.send(subscriberFound);
+        }
+    }catch(err){
+        res.status(400).send({messeage: err.messeage})
+    }
+    
+});
 
 
 
