9d5c44d00b24cec07d8117503e55152ebc38ff33
@devojyoti1995 <devojyoti1995@gmail.com>
2020-12-07 13:06:59 +0530

    Commit changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
-
+const subscribers = require('../src/models/subscribers.js')
 const express = require('express');
 const app = express()
 
 
 // Your code goes here
+app.get("/subscribers",(res,req)=>{
+    subscribers.findAll;
+})
+app.get("/subscribers/:id",(res,req)=>{
+    subscribers.findOne;
+})
+app.get("/subscribers/names",(res,req)=>{
+    subscribers.find(subscribers).find(names);
+})
 
 
 
